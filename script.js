b732a5f39d29fc7af42c39e529f8855ef62e8f2b
Jenny Sugden <jensugden@gmail.com>
2020-02-01 15:00:19 -0800

    added code

---
+++ script.js
@@ -0,0 +1,13 @@
+let result;
+let number1 = prompt("What is your first number?");
+let number2 = prompt("What is your second number?");
+
+console.log(number1);
+console.log(number2);
+
+function square(number1, number2) {
+  result = number1 * number2;
+}
+
+square(number1, number2);
+alert(result);
